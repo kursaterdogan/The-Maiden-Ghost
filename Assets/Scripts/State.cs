308a8d25358362d6dd02131f01ac65c9a8ccfc16
kursaterdogan <soullreaver@icloud.com>
2020-08-16 23:34:53 +0300

    Update State.cs

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -8,19 +8,12 @@ public class State : ScriptableObject
 {
     [SerializeField] int left;
     [SerializeField] int right;
-    [SerializeField] int current;
-
 
     [TextArea(5, 15)] [SerializeField] string storyText;
 
     public string GetStateStory()
     {
         return storyText;
-    }
-
-    public int GetCurrentIndex()
-    {
-        return current;
     }
 
     public int GetLeftIndex()
