f77cb72b92e600904f73fc0a482631baad5ee464
kursaterdogan <soullreaver@icloud.com>
2020-08-16 21:25:12 +0300

    Create Screenshot

---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class Screenshot : MonoBehaviour
+{
+    private int count;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ScreenCapture.CaptureScreenshot("Screenshot " + count.ToString() + ".png");
+            Debug.Log("Screenshot " + count.ToString());
+            count++;
+        }
+    }
+}
